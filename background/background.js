d31f14990a6dde3afdd095d9533f2361d25c04ed
Kadauchi <kadauchi@gmail.com>
2018-01-24 06:49:34 -0800

    added update check and storage validator

--- background/background.js
+++ background/background.js
@@ -1,3 +1,19 @@
+chrome.runtime.onUpdateAvailable.addListener((details) => {
+    chrome.browserAction.setBadgeText({
+        text: `↺`
+    });
+    chrome.browserAction.setBadgeBackgroundColor({
+        color: `#28a745`
+    });
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        const func = request.function;
+
+        if (request.checkForUpdateAvailable) {
+            sendResponse(true);
+        }
+    });
+});
+
 //********** Context Menus **********//
 chrome.contextMenus.create({
     title: `Paste Mturk Worker ID`,
@@ -80,6 +96,9 @@ chrome.omnibox.onInputEntered.addListener((text) => {
         });
     });
 });
+
+//https://worker.mturk.com/projects/*/tasks/*/submit
+//https://worker.mturk.com/projects/*/tasks/*/submit_qap
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const func = request.function;
@@ -955,111 +974,212 @@ function requesterReviewsUpdate(objectReviews, arrayIds) {
     });
 }
 
-
 //********** Storage  **********//
-const storage = new Object();
+const storage = {
+    blockList() {
+        const [object] = arguments, template = {};
 
-(async () => {
-    const items = await new Promise((resolve) => chrome.storage.local.get([`exports`, `reviews`, `scripts`, `themes`, `version`, `workerID`], resolve));
+        for (const key in object) {
+            const prop = object[key];
 
-    ((object) => {
-        const themes = [`mts`, `mturk`];
-
-        storage.themes = new Object();
-
-        if (object !== undefined) {
-            for (const value of themes) {
-                storage.themes[value] = object[value] !== undefined ? object[value] : `default`;
-            }
-        } else {
-            for (const value of themes) {
-                storage.themes[value] = `default`;
+            if (prop.match) {
+                template[prop.match] = {
+                    "name": prop.name || prop.match,
+                    "match": prop.match,
+                    "strict": typeof(prop.strict) === `boolean` ? prop.strict : true,
+                };
             }
         }
 
-        chrome.storage.local.set({
-            themes: storage.themes
-        });
-    })(items.themes);
+        storage.blockList = template;
+    },
 
+    includeList() {
+        const [object] = arguments, template = {};
 
-    ((string) => {
-        const exports = [`all`, `short`, `plain`, `bbcode`, `markdown`, `turkerhub`, `mturkcrowd`];
+        for (const key in object) {
+            const prop = object[key];
 
-        storage.exports = exports.includes(string) === true ? string : `all`;
-
-        chrome.storage.local.set({
-            exports: storage.exports
-        });
-    })(items.exports);
-
-    ((object) => {
-        const reviews = [`turkerview`, `turkopticon`, `turkopticon2`];
-
-        storage.reviews = new Object();
-
-        if (object !== undefined) {
-            for (const value of reviews) {
-                storage.reviews[value] = object[value] !== undefined ? object[value] : true;
-            }
-        } else {
-            for (const value of reviews) {
-                storage.reviews[value] = true;
+            if (prop.match) {
+                template[prop.match] = {
+                    "name": prop.name || prop.match,
+                    "match": prop.match,
+                    "strict": typeof(prop.strict) === `boolean` ? prop.strict : true,
+                    "sound": typeof(prop.sound) === `boolean` ? prop.sound : true,
+                    "alarm": typeof(prop.alarm) === `boolean` ? prop.alarm : false,
+                    "pushbullet": typeof(prop.pushbullet) === `boolean` ? prop.pushbullet : false,
+                    "notification": typeof(prop.notification) === `boolean` ? prop.notification : true
+                };
             }
         }
 
-        chrome.storage.local.set({
-            reviews: storage.reviews
-        });
-    })(items.reviews);
+        storage.includeList = template;
+    },
 
-    ((object) => {
-        const scripts = [
-            `autoAcceptChecker`, `blockListOnMturk`, `confirmReturnHIT`, `dashboardEnhancer`, `hitExporter`, `hitTracker`, `hitDetailsEnhancer`, `paginationLastPage`, `queueInfoEnhancer`, `rateLimitReloader`, `rememberFilter`, `requesterReviews`, `workspaceExpander`
-        ];
+    exports() {
+        const [string] = arguments, template = `all`;
 
-        storage.scripts = new Object();
+        storage.exports = [`all`, `short`, `plain`, `bbcode`, `markdown`, `turkerhub`, `mturkcrowd`].includes(string) ? string : template;
+    },
 
-        if (object !== undefined) {
-            for (const value of scripts) {
-                storage.scripts[value] = object[value] !== undefined ? object[value] : true;
-            }
-        } else {
-            for (const value of scripts) {
-                storage.scripts[value] = true;
+    hitFinder() {
+        const [object] = arguments, template = {
+            "speed": `3000`,
+
+            "filter-search-term": ``,
+            "filter-sort": `updated_desc`,
+            "filter-page-size": `25`,
+            "filter-masters": false,
+            "filter-qualified": false,
+            "filter-min-reward": `0`,
+            "filter-min-available": `0`,
+            "filter-min-requester-rating": `0`,
+
+            "alert-delay": `30`,
+            "alert-new-sound": `sound-1`,
+            "alert-include-sound": `voice`,
+            "alert-pushbullet-state": `off`,
+            "alert-pushbullet-token": ``,
+
+            "display-colored-rows": true,
+
+            "display-recent-column-time": false,
+            "display-recent-column-requester": true,
+            "display-recent-column-title": true,
+            "display-recent-column-available": true,
+            "display-recent-column-reward": true,
+            "display-recent-column-masters": true,
+            
+            "display-logged-column-time": true,
+            "display-logged-column-requester": true,
+            "display-logged-column-title": true,
+            "display-logged-column-available": true,
+            "display-logged-column-reward": true,
+            "display-logged-column-masters": true,
+            
+            "display-included-column-time": true,
+            "display-included-column-requester": true,
+            "display-included-column-title": true,
+            "display-included-column-available": true,
+            "display-included-column-reward": true,
+            "display-included-column-masters": true,
+        };
+
+        for (const prop in template) {
+            if (object && object[prop] && typeof(object[prop]) === typeof(template[prop])) {
+                template[prop] = object[prop];
             }
         }
 
-        chrome.storage.local.set({
-            scripts: storage.scripts
-        });
-    })(items.scripts);
+        storage.hitFinder = template;
+    },
 
-    ((string) => {
-        const version = chrome.runtime.getManifest().version;
+    reviews() {
+        const [object] = arguments, template = {
+            "turkerview": true,
+            "turkopticon": true,
+            "turkopticon2": true
+        };
 
-        if (string !== version) {
+        for (const prop in template) {
+            if (object && object[prop] && typeof(object[prop]) === typeof(template[prop])) {
+                template[prop] = object[prop];
+            }
+        }
+
+        storage.reviews = template;
+    },
+
+    scripts() {
+        const [object] = arguments, template = {
+            autoAcceptChecker: true,
+            blockListOnMturk: true,
+            confirmReturnHIT: true,
+            dashboardEnhancer: true,
+            hitExporter: true,
+            hitTracker: true,
+            hitDetailsEnhancer: true,
+            paginationLastPage: true,
+            queueInfoEnhancer: true,
+            rateLimitReloader: true,
+            rememberFilter: true,
+            requesterReviews: true,
+            workspaceExpander: true
+        };
+
+        for (const prop in template) {
+            if (object && object[prop] && typeof(object[prop]) === typeof(template[prop])) {
+                template[prop] = object[prop];
+            }
+        }
+
+        storage.scripts = template;
+    },
+
+    themes() {
+        const [object] = arguments, template = {
+            mts: `default`,
+            mturk: `default`
+        };
+
+        for (const prop in template) {
+            if (object && object[prop] && typeof(object[prop]) === typeof(template[prop])) {
+                template[prop] = object[prop];
+            }
+        }
+
+        storage.themes = template;
+    },
+
+    version() {
+        const [string] = arguments, template = chrome.runtime.getManifest().version;
+
+        if (string !== template) {
             chrome.tabs.create({
                 url: `/change_log/change_log.html`
             });
-            chrome.storage.local.set({
-                version: version
-            });
         }
-    })(items.version);
 
-    ((string) => {
-        storage.workerID = string || `A-------------`;
-    })(items.workerID);
+        storage.version = template;
+    },
 
-    chrome.storage.onChanged.addListener((changes) => {
-        for (const value of [`reviews`, `scripts`, `workerID`]) {
-            if (changes[value] !== undefined) {
-                storage[value] = changes[value].newValue;
+    workerId() {
+        const [string] = arguments, template = `A-------------`;
+
+        storage.workerId = string || template;
+    },
+
+    addListener() {
+        chrome.storage.onChanged.addListener((changes) => {
+            for (const value of [`reviews`, `scripts`, `workerID`]) {
+                if (changes[value] !== undefined) {
+                    storage[value] = changes[value].newValue;
+                }
             }
+        });
+
+        delete storage.addListener;
+    }
+};
+
+chrome.storage.local.get(null, (keys) => {
+    for (const prop in keys) {
+        if (storage[prop] && typeof(storage[prop]) === `function`) {
+            storage[prop](keys[prop]);
         }
-    });
-})();
+    } 
+
+    for (const prop in storage) {
+        if (typeof(storage[prop]) === `function`) {
+            storage[prop]();
+        }
+
+        chrome.storage.local.set({
+            [prop]: storage[prop]
+        });
+    }
+});
+
 
 //********** HIT Tracker **********//
 let hitTrackerDB;
