e7d1d552c2b562b3835cc8fd33a6f9bcdb54363e
Kadauchi <kadauchi@gmail.com>
2018-01-24 06:48:50 -0800

    refactor

--- mturk/workspaceExpander.js
+++ mturk/workspaceExpander.js
@@ -1,8 +1,11 @@
 (async () => {
-    await ready({ enabled: `workspaceExpander`, document: `interactive` });
+  await ready({ enabled: `workspaceExpander`, document: `interactive` })
 
-    const workspace = document.getElementsByClassName(`task-question-iframe-container`)[0];
-    workspace.style.height = `100vh`;
-    workspace.children[0].focus();
-    workspace.scrollIntoView();
-})();
+  const workspace = document.getElementsByClassName(`task-question-iframe-container`)[0] || document.getElementById(`hit-wrapper`)
+
+  if (workspace) {
+    workspace.style.height = `100vh`
+    workspace.children[0].focus()
+    workspace.scrollIntoView()
+  }
+})()
