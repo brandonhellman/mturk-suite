7aa326a320d3ecd15f117409c6c2d4b1546cdd3b
Kadauchi <kadauchi@gmail.com>
2018-01-24 04:54:33 -0800

    added theme

---
+++ js/theme.js
@@ -0,0 +1,15 @@
+(function updateTheme () {
+  const theme = document.createElement(`link`)
+  theme.rel = `stylesheet`
+  document.head.prepend(theme)
+
+  chrome.storage.local.get([`themes`], (keys) => {
+    theme.href = `/bootstrap/css/${keys.themes.mts}.min.css`
+
+    chrome.storage.onChanged.addListener((changes) => {
+      if (changes.themes) {
+        theme.href = `/bootstrap/css/${changes.themes.newValue.mts}.min.css`
+      }
+    })
+  })
+})()
