3cbedbecbabde6e172caab0b7b4af31f082a84b3
Kadauchi <kadauchi@gmail.com>
2018-01-27 04:48:29 -0800

    refactor

--- js/theme.js
+++ js/theme.js
@@ -1,12 +1,12 @@
-(function updateTheme () {
+(function () {
   const theme = document.createElement(`link`)
   theme.rel = `stylesheet`
   document.head.prepend(theme)
 
-  chrome.storage.local.get([`themes`], (keys) => {
+  window.chrome.storage.local.get([`themes`], (keys) => {
     theme.href = `/bootstrap/css/${keys.themes.mts}.min.css`
 
-    chrome.storage.onChanged.addListener((changes) => {
+    window.chrome.storage.onChanged.addListener((changes) => {
       if (changes.themes) {
         theme.href = `/bootstrap/css/${changes.themes.newValue.mts}.min.css`
       }
