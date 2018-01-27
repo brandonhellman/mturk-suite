8e84da1db5dc1aaf1f67e72bae55ff326c74031c
Kadauchi <kadauchi@gmail.com>
2018-01-27 05:02:53 -0800

    updated queue info enhancer for mturk changes

---
+++ mturk/queue-info-enhancer.js
@@ -0,0 +1,12 @@
+(async () => {
+  await ready({ enabled: `queueInfoEnhancer` })
+
+  const react = await require(`reactComponents/taskQueueTable/TaskQueueTable`)
+
+  const total = react.reactProps.bodyData
+  .map((currentValue) => currentValue.project.monetary_reward.amount_in_dollars)
+  .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+  const header = document.getElementsByClassName(`task-queue-header`)[0].getElementsByClassName(`m-b-0`)[0]
+  header.textContent = `${header.textContent.trim().slice(0, -1)} - $${total.toFixed(2)})`
+})()
