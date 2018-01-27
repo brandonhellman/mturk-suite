cdbf31b9e5542a419cf31e9910fe6baed9f51f9d
Kadauchi <kadauchi@gmail.com>
2018-01-27 05:19:35 -0800

    refactor

--- mturk/queue-info-enhancer.js
+++ mturk/queue-info-enhancer.js
@@ -2,11 +2,8 @@
   await ready({ enabled: `queueInfoEnhancer` })
 
   const react = await require(`reactComponents/taskQueueTable/TaskQueueTable`)
-
-  const total = react.reactProps.bodyData
-  .map((currentValue) => currentValue.project.monetary_reward.amount_in_dollars)
-  .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  const queueValue = react.reactProps.bodyData.reduce((accumulator, currentValue) => accumulator + currentValue.project.monetary_reward.amount_in_dollars, 0)
 
   const header = document.getElementsByClassName(`task-queue-header`)[0].getElementsByClassName(`m-b-0`)[0]
-  header.textContent = `${header.textContent.trim().slice(0, -1)} - $${total.toFixed(2)})`
+  header.textContent = `${header.textContent.trim().slice(0, -1)} - $${queueValue.toFixed(2)})`
 })()
