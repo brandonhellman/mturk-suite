acd8952e08eae6cd2cb6c8220b6c680aab8f2b53
Kadauchi <kadauchi@gmail.com>
2018-01-30 13:40:18 -0800

    removed console message

--- js/mturk-script-enabled.js
+++ js/mturk-script-enabled.js
@@ -5,7 +5,6 @@ function scriptEnabled () {
 
   return new Promise((resolve) => {
     (function checkStorage () {
-      console.log(storage)
       if (storage && storage.scripts && storage.readyState === `complete`) {
         resolve(storage.scripts[script])
       } else {
