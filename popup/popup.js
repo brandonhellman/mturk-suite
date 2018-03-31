adeee3f07563abd7e3330d7fef7fa789ef622d31
Kadauchi <kadauchi@gmail.com>
2018-03-31 16:50:53 -0700

    Removed checkForUpdateAvailable()
    Updates are now handled a different way making this code obsolete.

--- popup/popup.js
+++ popup/popup.js
@@ -2,17 +2,4 @@
 
 (function getVersion () {
   document.getElementById(`version`).textContent = `v${chrome.runtime.getManifest().version}`
-})();
-
-(function checkForUpdateAvailable () {
-  chrome.runtime.sendMessage({ checkForUpdateAvailable: true }, (response) => {
-    if (response) {
-      const update = document.createElement(`button`)
-      update.className = `btn btn-sm btn-success fa fa-arrow-alt-circle-up float-right`
-      update.addEventListener(`click`, (event) => {
-        chrome.runtime.reload()
-      })
-      document.getElementById(`version`).parentElement.appendChild(update)
-    }
-  })
 })()
