cc6c361b3db2d06184f1b5fce4a7a449ea5bd788
Kadauchi <kadauchi@gmail.com>
2018-05-11 17:02:00 -0700

    Inital commit.

---
+++ background/omnibox.js
@@ -0,0 +1,13 @@
+chrome.omnibox.onInputChanged.addListener(text => {
+  chrome.omnibox.setDefaultSuggestion({
+    description: `Search Mechanical Turk for ${text}`
+  });
+});
+
+chrome.omnibox.onInputEntered.addListener(text => {
+  chrome.tabs.getSelected(null, tab => {
+    chrome.tabs.update(tab.id, {
+      url: ENCODE`https://worker.mturk.com/?filters%5Bsearch_term%5D=${text}`
+    });
+  });
+});
---
+++ mturk/theme.js
@@ -0,0 +1,27 @@
+function setTheme(style) {
+  const el = document.getElementById(`mts-theme`);
+  const href = chrome.extension.getURL(`bootstrap/css/${style}.worker.css`);
+
+  if (el) {
+    el.href = href;
+  } else {
+    document.head.insertAdjacentHTML(
+      `beforeend`,
+      HTML`<link rel="stylesheet" type="text/css" href="${href}" id="mts-theme">`
+    );
+  }
+}
+
+async function theme() {
+  const { themeMturk } = await StorageGetKey(`options`);
+
+  if (themeMturk !== `default`) setTheme(themeMturk);
+
+  chrome.storage.onChanged.addListener(changes => {
+    if (changes.options && changes.options.newValue.themeMturk) {
+      setTheme(themeMturk);
+    }
+  });
+}
+
+theme();
