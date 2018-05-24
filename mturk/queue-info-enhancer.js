44e01f211fbde02ab8b80580e936b62430a4f5be
Kadauchi <kadauchi@gmail.com>
2018-05-24 10:27:50 -0700

    Refactored.

--- mturk/queue-info-enhancer.js
+++ mturk/queue-info-enhancer.js
@@ -1,12 +1,18 @@
 async function queueInfoEnhancer() {
-  const props = await ReactProps(`TaskQueueTable`);
+  const [props] = await Promise.all([
+    ReactProps(`TaskQueueTable`),
+    Enabled(`queueInfoEnhancer`)
+  ]);
+
   const value = props.bodyData.reduce(
     (acc, cV) => acc + cV.project.monetary_reward.amount_in_dollars,
     0
   );
 
   const header = document.querySelector(`h1.m-b-0`);
-  header.textContent = `${header.textContent.trim().slice(0, -1)} - $${value.toFixed(2)})`;
+  header.textContent = `${header.textContent
+    .trim()
+    .slice(0, -1)} - $${value.toFixed(2)})`;
 }
 
-Enabled(queueInfoEnhancer);
+queueInfoEnhancer();
