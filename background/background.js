a33fe414b9df2b0196c206955d635ac1ffaf20bb
Kadauchi <kadauchi@gmail.com>
2018-05-23 21:03:49 -0700

    Added default setting for Finder.

--- background/background.js
+++ background/background.js
@@ -11,24 +11,25 @@ chrome.runtime.onUpdateAvailable.addListener(details => {
 });
 
 chrome.storage.local.get(`blockList`, keys => {
-  const { blockList } = keys;
-  if (!blockList) chrome.storage.local.set({ blockList: {} });
+  if (!keys.blockList) {
+    chrome.storage.local.set({ blockList: {} });
+  }
 });
 
 chrome.storage.local.get(`earnings`, keys => {
-  const { earnings } = keys;
-  if (!earnings) chrome.storage.local.set({ earnings: 0 });
+  if (!keys.earnings) {
+    chrome.storage.local.set({ earnings: 0 });
+  }
 });
 
 chrome.storage.local.get(`includeList`, keys => {
-  const { includeList } = keys;
-  if (!includeList) chrome.storage.local.set({ includeList: {} });
+  if (!keys.includeList) {
+    chrome.storage.local.set({ includeList: {} });
+  }
 });
 
 chrome.storage.local.get(`options`, keys => {
-  const { options } = keys;
-
-  if (!options || options.version !== 1) {
+  if (!keys.options || keys.options.version !== 1) {
     chrome.storage.local.set({
       options: {
         autoAcceptChecker: true,
@@ -52,6 +53,54 @@ chrome.storage.local.get(`options`, keys => {
         themeMts: `default`,
         themeMturk: `default`,
         workspaceExpander: true
+      }
+    });
+  }
+});
+
+chrome.storage.local.get(`hitFinder`, keys => {
+  if (!keys.hitFinder) {
+    chrome.storage.local.set({
+      hitFinder: {
+        speed: `3000`,
+
+        "filter-search-term": ``,
+        "filter-sort": `updated_desc`,
+        "filter-page-size": `25`,
+        "filter-masters": false,
+        "filter-qualified": false,
+        "filter-min-reward": `0`,
+        "filter-min-available": `0`,
+        "filter-min-requester-rating": `0`,
+
+        "alert-new-sound": `sound-1`,
+        "alert-include-delay": `30`,
+        "alert-include-sound": `voice`,
+        "alert-pushbullet-state": `off`,
+        "alert-pushbullet-token": ``,
+
+        "display-colored-rows": true,
+
+        "display-recent-column-time": false,
+        "display-recent-column-requester": true,
+        "display-recent-column-title": true,
+        "display-recent-column-available": true,
+        "display-recent-column-reward": true,
+        "display-recent-column-masters": true,
+
+        "display-logged-column-time": true,
+        "display-logged-column-requester": true,
+        "display-logged-column-title": true,
+        "display-logged-column-available": true,
+        "display-logged-column-reward": true,
+        "display-logged-column-masters": true,
+
+        "display-included-column-time": true,
+        "display-included-column-requester": true,
+        "display-included-column-title": true,
+        "display-included-column-available": true,
+        "display-included-column-reward": true,
+        "display-included-column-masters": true
       }
     });
   }
