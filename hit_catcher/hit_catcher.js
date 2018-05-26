c7e2d57e094b5fe1a2027d0bb933aebd8c6d12a7
Kadauchi <kadauchi@gmail.com>
2018-05-26 14:16:01 -0700

    Changed.
    Moved theme updating to external js like Tracker and Finder.

--- hit_catcher/hit_catcher.js
+++ hit_catcher/hit_catcher.js
@@ -1,17 +1,3 @@
-(function updateTheme() {
-    const theme = document.getElementById(`theme`);
-
-    chrome.storage.local.get([`themes`], (keys) => {
-        theme.href = `/bootstrap/css/${keys.themes.mts}.min.css`;
-
-        chrome.storage.onChanged.addListener((changes) => {
-            if (changes.themes) {
-                theme.href = `/bootstrap/css/${changes.themes.newValue.mts}.min.css`;
-            }
-        });
-    });
-})();
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const hitCatcher = request.hitCatcher;
 
