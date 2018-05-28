fe7983c4c806d6d022acfae006b5b5eef1f8ce21
Kadauchi <kadauchi@gmail.com>
2018-05-28 14:27:25 -0700

    Fixed #95

--- mturk/hit-tracker-display-earnings.js
+++ mturk/hit-tracker-display-earnings.js
@@ -11,8 +11,15 @@ async function hitTrackerDisplayEarnings() {
   const element = document.getElementsByClassName(`col-xs-7`)[0];
   element.insertAdjacentHTML(
     `beforeend`,
-    `<span> - Earnings: </span><a href="${href}" target="_blank">${money}</a>`
+    `<span> - Earnings: </span><a href="${href}" target="_blank" id="mts-ht-earnings">${money}</a>`
   );
+
+  chrome.storage.onChanged.addListener(changes => {
+    if (changes.earnings) {
+      const el = document.getElementById(`mts-ht-earnings`);
+      el.textContent = `$${changes.earnings.newValue.toFixed(2)}`;
+    }
+  });
 }
 
 hitTrackerDisplayEarnings();
