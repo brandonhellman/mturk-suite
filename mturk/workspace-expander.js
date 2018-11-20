87a48284fb4e0e31a05c5fc2a2a0dfa2fbd191ed
kadauchi <kadauchi@gmail.com>
2018-11-20 08:30:56 -0800

    Fix #166

--- mturk/workspace-expander.js
+++ mturk/workspace-expander.js
@@ -5,9 +5,18 @@ async function workspaceExpander() {
   ]);
 
   const workspace = document.querySelector('#MainContent');
-  workspace.style.height = `100vh`;
-  workspace.children[0].focus();
-  workspace.scrollIntoView();
+  const iframe = document.querySelector('.task-question-iframe-container');
+
+  if (workspace) {
+    workspace.style.height = `100vh`;
+    workspace.scrollIntoView();
+  }
+
+  if (iframe) {
+    iframe.style.height = `100vh`;
+    iframe.scrollIntoView();
+    iframe.querySelector('iframe').focus();
+  }
 
   if (options.hitDetailsSticky) {
     const detailBar = document.querySelector(`.project-detail-bar`);
