4ef4b33088e3779e64b8ac1c827f10e32891a31a
kadauchi <kadauchi@gmail.com>
2018-12-01 09:37:24 -0800

    Fixed #168
    - Expanded the task row and task preview to allow the iframe to be resized to 100%.
    - The offending footers have been moved elsewhere in the DOM to prevent floating into the workspace.

--- mturk/workspace-expander.js
+++ mturk/workspace-expander.js
@@ -1,10 +1,12 @@
 async function workspaceExpander() {
   const [options] = await Promise.all([
     StorageGetKey(`options`),
-    Enabled(`workspaceExpander`)
+    Enabled(`workspaceExpander`),
   ]);
 
   const workspace = document.querySelector('#MainContent');
+  const taskRow = document.querySelector('.task-row');
+  const taskPreview = document.querySelector('.task-preview');
   const iframe = document.querySelector('.task-question-iframe-container');
 
   if (workspace) {
@@ -12,11 +14,28 @@ async function workspaceExpander() {
     workspace.scrollIntoView();
   }
 
+  if (taskRow) {
+    taskRow.style.height = '100vh';
+    taskRow.scrollIntoView();
+  }
+
+  if (taskPreview) {
+    taskPreview.style.height = '100%';
+    taskPreview.scrollIntoView();
+  }
+
   if (iframe) {
-    iframe.style.height = `100vh`;
+    iframe.style.height = `100%`;
     iframe.scrollIntoView();
     iframe.querySelector('iframe').focus();
   }
+
+  const hr = document.querySelector('hr.footer-horizontal-rule');
+  const div = document.querySelector('div.work-pipeline-bottom-bar');
+  const footer = document.querySelector('footer');
+
+  document.body.insertBefore(hr, footer);
+  document.body.insertBefore(div, footer);
 
   if (options.hitDetailsSticky) {
     const detailBar = document.querySelector(`.project-detail-bar`);
