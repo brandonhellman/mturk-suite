aa1e4206667652c091d18cbd6ba7dd2163b8f5db
Kadauchi <kadauchi@gmail.com>
2019-08-22 16:37:52 -0700

    Return early if not enabled.

--- src/scripts/rateLimitReloader.ts
+++ src/scripts/rateLimitReloader.ts
@@ -7,11 +7,13 @@ const store = new Store();
 store.ready().then(() => {
   const options = selectOptions(store.getState());
 
-  if (options.scripts.rateLimitReloader) {
-    const error = document.querySelector('.error-page');
+  if (!options.scripts.rateLimitReloader) {
+    return;
+  }
 
-    if (error && error.textContent.includes('You have exceeded')) {
-      window.setTimeout(window.location.reload.bind(window.location), 1000);
-    }
+  const error = document.querySelector('.error-page');
+
+  if (error && error.textContent.includes('You have exceeded')) {
+    window.setTimeout(window.location.reload.bind(window.location), 1000);
   }
 });
