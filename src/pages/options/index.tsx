e2c3f13cfe67c57b64e41094c9246482614fa568
Kadauchi <kadauchi@gmail.com>
2019-08-22 15:40:49 -0700

    Connect to redux

--- src/pages/options/index.tsx
+++ src/pages/options/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { Store } from 'webext-redux';
 
 import { App } from './components/App';
 
 ReactDom.render(<App />, document.getElementById('root'));
+
+const store = new Store();
+
+store.ready().then(() => {
+  ReactDom.render(
+    // @ts-ignore
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
+  );
+});
