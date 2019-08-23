66da34ed49516b0b48db4d3c8a0fbcf99429784c
Kadauchi <kadauchi@gmail.com>
2019-08-23 12:33:53 -0700

    injectReviewPopover

--- src/scripts/containers/Turkerview.tsx
+++ src/scripts/containers/Turkerview.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { TurkerviewIcon } from '../../components/TurkerviewIcon';
 import { ReviewButton } from '../components/ReviewButton';
+import { injectReviewPopover } from '../../utils/injectReviewPopover';
 
 interface Props {
   rid: string;
 }
 
 export function Turkerview({ rid }: Props) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    injectReviewPopover(ref, rid, rid, <div>{rid}</div>);
+  }, []);
+
   return (
     <ReviewButton>
       <TurkerviewIcon />
+      <script ref={ref} />
     </ReviewButton>
   );
 }
--- src/scripts/containers/Turkopticon.tsx
+++ src/scripts/containers/Turkopticon.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { TurkopticonIcon } from '../../components/TurkopticonIcon';
 import { ReviewButton } from '../components/ReviewButton';
+import { injectReviewPopover } from '../../utils/injectReviewPopover';
 
 interface Props {
   rid: string;
 }
 
 export function Turkopticon({ rid }: Props) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    injectReviewPopover(ref, rid, rid, <div>{rid}</div>);
+  }, []);
+
   return (
     <ReviewButton>
       <TurkopticonIcon />
+      <script ref={ref} />
     </ReviewButton>
   );
 }
