35f923e0ecee3481a1a2416d270282074bd159cf
Kadauchi <kadauchi@gmail.com>
2019-08-23 16:00:37 -0700

    Update injectReviewPopover.ts

--- src/utils/injectReviewPopover.ts
+++ src/utils/injectReviewPopover.ts
@@ -2,15 +2,15 @@ import ReactDOMServer from 'react-dom/server';
 
 export function injectReviewPopover(
   ref: React.MutableRefObject<HTMLScriptElement>,
-  rid: string,
-  rname: string,
+  requester_id: string,
+  requester_name: string,
   content: React.ReactElement,
 ) {
   const script = document.createElement('script');
   script.textContent = `$(document.currentScript).parent().popover({
     html: true,
     trigger: 'hover focus',
-    title: '${rname} [${rid}]',
+    title: '${requester_name} [${requester_id}]',
     content: '${ReactDOMServer.renderToString(content)}'
   });`;
   ref.current.replaceWith(script);
