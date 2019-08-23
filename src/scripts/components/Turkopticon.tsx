741d9ba763bcc184033bd8252923d904e2b8844b
Kadauchi <kadauchi@gmail.com>
2019-08-23 15:32:47 -0700

    Update Turkopticon.tsx

--- src/scripts/components/Turkopticon.tsx
+++ src/scripts/components/Turkopticon.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 
 import { TurkopticonIcon } from '../../components/TurkopticonIcon';
-import { TurkopticonPopover } from '../containers/TurkopticonPopover';
 import { injectReviewPopover } from '../../utils/injectReviewPopover';
+import { TurkopticonPopover } from '../containers/TurkopticonPopover';
 import { ReviewButton } from './ReviewButton';
 
 interface Props {
