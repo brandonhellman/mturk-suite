51218ac93d1d9a4431b20b0f1d0165b0994c0f04
Kadauchi <kadauchi@gmail.com>
2019-08-23 14:51:13 -0700

    Only dispatch the workerId if its different.

--- src/scripts/getWorkerId.ts
+++ src/scripts/getWorkerId.ts
@@ -1,11 +1,16 @@
 import { Store } from 'webext-redux';
 
 import { mturkUpdate } from '../store/mturk/actions';
+import { selectMturk } from '../store/mturk/selectors';
 import { getReactProps, ReactPropsCopyText } from '../utils/getReactProps';
 
 const store = new Store();
 
 store.ready().then(async () => {
+  const mturk = selectMturk(store.getState());
   const props: ReactPropsCopyText = await getReactProps('CopyText');
-  store.dispatch(mturkUpdate({ key: 'workerId', value: props.textToCopy }));
+
+  if (mturk.workerId !== props.textToCopy) {
+    store.dispatch(mturkUpdate({ key: 'workerId', value: props.textToCopy }));
+  }
 });
