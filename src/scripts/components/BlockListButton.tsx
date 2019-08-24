17311e0cb68103bae4871ca9c4ef5afc90459a37
Kadauchi <kadauchi@gmail.com>
2019-08-24 10:55:04 -0700

    Create BlockListButton.tsx

---
+++ src/scripts/components/BlockListButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function BlockListButton() {
+  return (
+    <button type="button" className="btn btn-primary m-l-lg" data-toggle="modal" data-target="#block-list-modal">
+      Block List
+    </button>
+  );
+}
