fc00423061ca777e16773c7cdd1164df465355f6
Kadauchi <kadauchi@gmail.com>
2019-08-23 18:36:05 -0700

    Display the total reward of the queue.

---
+++ src/scripts/queueInfoEnhancer.ts
@@ -0,0 +1,20 @@
+import { Store } from 'webext-redux';
+
+import { selectOptions } from '../store/options/selectors';
+import { getReactProps, ReactPropsTaskQueueTable } from '../utils/getReactProps';
+
+const store = new Store();
+
+store.ready().then(async () => {
+  const options = selectOptions(store.getState());
+
+  if (!options.scripts.queueInfoEnhancer) {
+    return;
+  }
+
+  const props: ReactPropsTaskQueueTable = await getReactProps('TaskQueueTable');
+  const reward = props.bodyData.reduce((acc, task) => acc + task.project.monetary_reward.amount_in_dollars, 0);
+
+  const header = document.querySelector(`h1.m-b-0`);
+  header.textContent = `${header.textContent.trim().slice(0, -1)} - $${reward.toFixed(2)})`;
+});
