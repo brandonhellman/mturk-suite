7a27c0feffb7b54cc5393a08f26aeb80fe5ba567
Kadauchi <kadauchi@gmail.com>
2019-12-09 08:33:32 -0800

    Update App.tsx

--- src/pages/options/components/App.tsx
+++ src/pages/options/components/App.tsx
@@ -1,32 +1,14 @@
-import React, { useState } from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Tab from 'react-bootstrap/Tab';
+import React from 'react';
 
+import { OptionsPane } from './OptionsPane';
 import { ChangeLogPane } from './ChangeLogPane';
-import { OptionsPane } from '../containers/OptionsPane';
+import Theme from '../../../components/Theme';
 
 export function App() {
-  const [key, setKey] = useState('options');
-
-  function onSelect(k: string) {
-    setKey(k);
-  }
-
   return (
-    <Tab.Container activeKey={key} onSelect={onSelect}>
-      <Nav variant="pills">
-        <Nav.Item >
-          <Nav.Link eventKey="options">Options</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="change-log">Change Log</Nav.Link>
-        </Nav.Item>
-      </Nav>
-
-      <Tab.Content>
-        <OptionsPane />
-        <ChangeLogPane />
-      </Tab.Content>
-    </Tab.Container>
+    <Theme>
+      <OptionsPane />
+      <ChangeLogPane />
+    </Theme>
   );
 }
