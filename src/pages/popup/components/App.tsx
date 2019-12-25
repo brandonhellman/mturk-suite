e1d2620e8fe89fab2381334db3648a4acbd9b102
Brandon Hellman <me@brandonhellman.com>
2019-12-25 15:34:01 -0800

    Link to the new pages.

--- src/pages/popup/components/App.tsx
+++ src/pages/popup/components/App.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+const buttonStyle = {
+  width: 200,
+};
+
 export default function App() {
-  return <h1>Hello from app.tsx!!</h1>;
+  return (
+    <>
+      <a className="btn btn-primary mb-1" href="/pages/hit_finder/index.html" style={buttonStyle} target="_blank">
+        HIT Finder
+      </a>
+      <a className="btn btn-primary mb-1" href="/pages/hit_catcher/index.html" style={buttonStyle} target="_blank">
+        HIT Catcher
+      </a>
+      <a className="btn btn-primary" href="/pages/hit_tracker/index.html" style={buttonStyle} target="_blank">
+        HIT Tracker
+      </a>
+    </>
+  );
 }
