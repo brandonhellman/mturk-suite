f6313d7ac1a4d1983b08a5ae6a31ddff96b67115
Brandon Hellman <me@brandonhellman.com>
2019-12-25 15:12:37 -0800

    Added theme to the popup.

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { Store } from 'webext-redux';
+
+import Theme from '../../components/Theme';
 
 import App from './components/App';
 
-ReactDom.render(<App />, document.getElementById('root'));
+const store = new Store();
+
+store.ready().then(() => {
+  ReactDom.render(
+    <Provider store={store}>
+      <Theme>
+        <App />
+      </Theme>
+    </Provider>,
+    document.getElementById('root'),
+  );
+});
