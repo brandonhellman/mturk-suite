74dfaed47bf69ea9b7edef1ecf89e9e127503f0c
Brandon Hellman <me@brandonhellman.com>
2019-12-25 16:21:14 -0800

    Added version and options link in a header.

--- src/pages/popup/components/App.tsx
+++ src/pages/popup/components/App.tsx
@@ -1,4 +1,7 @@
+import { browser } from 'webextension-polyfill-ts';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 const buttonStyle = {
   width: 200,
@@ -7,6 +10,14 @@ const buttonStyle = {
 export default function App() {
   return (
     <>
+      <div className="d-flex bg-primary text-white mb-1 p-1">
+        <div className="flex-grow-1 align-self-center">
+          Mturk Suite <small>v{browser.runtime.getManifest().version}</small>
+        </div>
+        <a className="btn btn-sm text-white" href="/pages/options/index.html" target="_blank">
+          <FontAwesomeIcon icon={faCog} />
+        </a>
+      </div>
       <a className="btn btn-primary mb-1" href="/pages/hit_finder/index.html" style={buttonStyle} target="_blank">
         HIT Finder
       </a>
